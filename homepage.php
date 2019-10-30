050388de8ac3d2dc994ac04846f032da55f6005b
Blake <blake.sweet16@gmail.com>
2019-10-30 10:41:45 -0400

    Added redirect and styling

--- homepage.php
+++ homepage.php
@@ -1,19 +1,23 @@
 <html>
-<body>
-<Title>Student Search Webpage</Title>
+	<head>
+		<title>Homepage</title>
+		<link rel="stylesheet" href="style.css"> <!-- Added CSS Styling -->
+	</head>
+	<body>
+		<Title>Student Search Webpage</Title>
 
-<b>Welcome to the student search webpage!</b>
+		<b>Welcome to the student search webpage!</b>
 
-Please enter the professor for which you would like to his or her students!
+		<br/>Please enter the professor for which you would like to his or her students!
 
-<form action="search_query.php" method="post">
-Professor: <input type="text" name="professor">
-<input type="submit">
-</form>
+		<form action="search_query.php" method="post">
+		Professor: <input type="text" name="professor">
+		<input type="submit">
+		</form>
 
-<form action="signup.php" method="post">
-<input type="submit" value="SIGN UP HERE!">
-</form>
+		<form action="signup.php" method="post">
+		<input type="submit" value="SIGN UP HERE!">
+		</form>
 
-</body>
+	</body>
 </html>
