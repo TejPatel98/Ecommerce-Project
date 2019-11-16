6742f470e1287bb96b14206a619942e55b1bd635
toucankhan <tam.k.nguyen14@gmail.com>
2019-11-15 19:09:13 -0500

    added Bootstrap and inventory view to manager page with promotions, submit button. Still needs mysql query to update database with new promo prices

--- Manager.php
+++ Manager.php
@@ -1,43 +1,72 @@
-<?php
-include('connect.php');
-/*
-$servername = "localhost";
-$username = "tej";
-$password = "hellophpworld";
-$dbname = "eCommerce";
+<!DOCTYPE html>
+<html>
+    <?php
+        include('connect.php');
+    ?>
+    <head>
+        <title>Manager Page</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="container">
+            <br>
+            <h2>
+                <?php 
+                    $manager_name = $_GET["identification"];
+                    echo "Welcome, ", $manager_name;
+                ?>
+            </h2>
+            <p>View and update inventory, ship pending orders, and view sales statistics and promotions below.</p>
+            <h5>Inventory</h5>          
+            <table class="table table-dark">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Cost</th>
+                        <th>Quantity</th>
+                        <th>Promotions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        $sql = "select * from Product;";
+                        $result = mysqli_query($conn, $sql);
+                        $data = array();
 
-echo "You are manager!";
-
-// Create connection 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection 
-if (!$conn){
-        die("Connection failed: " . mysqli_connect_error());
-}
- */
-$sql = "select * from Product;";
-$result = mysqli_query($conn, $sql);
-$data = array();
-
-echo '<table border="0" cellspacing="2" cellpadding="2">
-      <tr>
-          <td> <font face="Arial">Name</font> </td>
-          <td> <font face="Arial">Cost</font> </td>
-          <td> <font face="Arial">Quantity</font> </td>
-      </tr>';
-
-
-while ($row = mysqli_fetch_assoc($result)) {
-
-	echo '<tr>
-                  <td>'.$row['name'].'</td>
-                  <td>'.$row['cost'].'</td>
-              	  <td>'.$row['Quantity'].'</td>
-              </tr>';
-
-}
-
-
-
-?>
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo '<tr>
+                                        <td>'.$row['name'].'</td>
+                                        <td>'.$row['cost'].'</td>
+                                        <td>'.$row['Quantity'].'</td>
+                                        <td>
+                                            <div class="dropdown show">
+                                                <button class="btn btn-secondary dropdown-toggle" href="#" type="button" id="promoDropdown'.$row['productId'].'" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                    None
+                                                </button>
+                                                <ul class="dropdown-menu" aria-labelledby="promoDropdown">
+                                                    <li><a class="dropdown-item" data-value=".95" href="#">5% off</a></li>
+                                                    <li><a class="dropdown-item" data-value=".90" href="#">10% off</a></li>
+                                                    <li><a class="dropdown-item" data-value=".50" href="#">50% off</a></li>
+                                                </ul>
+                                            </div>
+                                        </td>
+                                    </tr>';
+                        }
+                    ?>
+                    <script>
+                        $(".dropdown-menu li a").click(function(){
+                            $(this).parents(".dropdown").find('.btn').html($(this).text());
+                            $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+                        });
+                    </script>
+                </tbody>
+            </table>
+            <button type="button" class="btn btn-default">Update Promotions</button>
+        </div>
+    </body>
+</html>
